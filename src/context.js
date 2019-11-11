f7b995d20ee3eb29909cad96d2c8c0305394a4e7
Danny Bentley <danny.bentley@som.com>
2019-11-10 17:29:48 -0800

    finished the search filter drop down.

--- src/context.js
+++ src/context.js
@@ -53,22 +53,45 @@ export default class RoomProvider extends Component {
   };
 
   handleChange = event => {
-    const type = event.target.type;
+    const target = event.target;
+    const value = event.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
-    console.log(type,name,value);
-  }
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
+  };
 
-  filterRooms = () =>{
-    console.log('hello')
-  }
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    let tempRooms = [...rooms];
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+
+    this.setState({
+      sortedRooms: tempRooms
+    });
+  };
   render() {
     return (
       <RoomContext.Provider
         value={{
           ...this.state,
           getRoom: this.getRoom,
-          handleChange:this.handleChange
+          handleChange: this.handleChange
         }}
       >
         {this.props.children}
